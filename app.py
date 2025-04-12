0fa0832d817f4f7843e8c2ae033958e2a44eb712
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-04-12 10:09:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'utils')))
+from preprocessing import preprocess_data
 import streamlit as st
 import pandas as pd
 import numpy as np
